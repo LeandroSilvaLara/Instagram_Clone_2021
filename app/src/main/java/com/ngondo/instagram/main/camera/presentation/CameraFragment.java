f0d9043d464737b22138ee6ea2dbf9a46e4f8969
Leandro <leandrodasilvalara@yahoo.com.br>
2021-10-24 16:27:18 -0300

    Finalizando parte do layout

---
+++ app/src/main/java/com/ngondo/instagram/main/camera/presentation/CameraFragment.java
@@ -0,0 +1,94 @@
+package com.ngondo.instagram.main.home.presentation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ngondo.instagram.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState
+    ) {
+        View view = inflater.inflate(R.layout.fragment_main_home, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.home_recycler);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        recyclerView.setAdapter(new PostAdapter());
+
+        return view;
+    }
+
+    private static class PostViewHolder extends RecyclerView.ViewHolder{
+        private final ImageView imagePost;
+
+        public PostViewHolder(@NonNull View itewView) {
+            super(itewView);
+            imagePost = itemView.findViewById(R.id.profile_image_grid);
+        }
+
+        public void bind(int image) {
+            this.imagePost.setImageResource(image);
+        }
+    }
+
+    private class PostAdapter extends RecyclerView.Adapter<PostViewHolder> {
+
+        private final int[] images = new int[] {
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add,
+                R.drawable.ic_insta_add
+        };
+
+        @NonNull
+        @Override
+        public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new PostViewHolder(getLayoutInflater().inflate(R.layout.item_post_list, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull HomeFragment.PostViewHolder holder, int position) {
+            holder.bind(images[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return images.length;
+        }
+    }
+
+    }
+
